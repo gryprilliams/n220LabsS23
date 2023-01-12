eafff313d747b293a139e097a2ac7afb419c7a38
gryprilliams <gageryanprince@gmail.com>
2023-01-12 15:54:16 -0500

    Creating base project structure

---
+++ labs/lab0/js/app.js
@@ -0,0 +1,11 @@
+function setup () {
+    createCanvas(800,600);
+    background(252,90,141);
+
+    fill (0,0,0);
+    rect (40,50,100,20);
+}
+
+function draw() {
+    circle(mouseX, mouseY, 30);
+}
